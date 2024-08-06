0a2768948165dfd000b771abd15845129807eff4
James Joseph <98562132+JW796@users.noreply.github.com>
2024-08-06 11:23:07 -0400

    Bug fix and updated styles w module setup

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -2,23 +2,25 @@ import { useState } from "react"
 
 
 const AddTask = () => {
-    const (addModal, setModal) = useState(false);
+    const [addModal, setAddModal] = useState(false);
 
     return (
         <>
         <button className="bg-blue-500 text-white uppercase text-sm font-semibold py-1 mx-1.5 pl-2 pr-2.5 rounded hover:opacity-70"
             type="button"
-            onClick={() => setModal(true)}
+            onClick={() => setAddModal(true)}
             >
             + New Task</button>
             {addModal ? (
                 <>
-                <div>
-                    <h3>Add New Task</h3>
+                <div className="flex items-center justify-center overflow-x-hidden overflow-y-auto fixed inset-0 z-100">
+                    <h3 className="bg-white">Add New Task</h3>
                 </div>
-                
+                 
                 </>
             ) : null}
         </>
     )
 }
+
+export default AddTask;
