0e2cef0b2578fcc43c985d604d1dc39cc6fadb39
James Joseph <98562132+JW796@users.noreply.github.com>
2024-08-07 13:36:02 -0400

    Render Project name and Description input

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -14,7 +14,44 @@ const AddTask = () => {
             {addModal ? (
                 <>
                 <div className="flex items-center justify-center overflow-x-hidden overflow-y-auto fixed inset-0 z-100">
-                    <h3 className="bg-white">Add New Task</h3>
+                    <div className="w-9/12 bg-white rounded-tg shaddow-md relative flex flex-col">
+                    <div className="flex flex-row justify-between p-5">
+                    <h3 className="text-3xl font-semibold">Add New Task</h3>
+                    <button
+                    className="px-1 text-gray-400 float-right text-3xl leading-none font-semibold block"
+                    onClick={() => setAddModal(false)}
+                    >
+                        x
+                    </button>
+                    </div>
+                    <form className="p-6">
+                        <div>
+
+                        <label className="track-wide uppercase text-gray-700 text-xx font-semibold mb-2 block"
+                        htmlFor="project-name">
+                            Project Name
+                        </label>
+                            <input
+                            className="w-full big-gray-200 test-grey-700 border border-gray-200 rounded py-3 px-4 mb-5 leading-tight focus:outline-none focus:bg-white"
+                                id="project-name"
+                                type="text" 
+                                placeholder="Project Name"
+                                required
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor=""></label>
+                            <textarea 
+                            className="w-full big-gray-200 test-grey-700 border border-gray-200 rounded py-3 px-4 mb-5 leading-tight focus:outline-none focus:bg-white" 
+                            id="task-description"
+                            rows="3"
+                            placeholder="Task Description"
+                            >
+
+                            </textarea>
+                        </div>
+                        </form>
+                    </div>
                 </div>
                  
                 </>
