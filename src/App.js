9d55262eefa042efbfacf76fdffe121557ed50d6
James Joseph <98562132+JW796@users.noreply.github.com>
2024-12-10 11:07:21 -0500

    WIP config & drag + drpp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import { useState } from "react";
-import AddTask from "./components/AddTask"
+import { useEffect, useState } from "react";
+import { useDrop } from "react-dnd";
+import AddTask from "./components/AddTask";
 import ToDo from "./components/ToDo";
 
 function App() {
-  const {taskList, setTaskList} = useState([]);
+  const { taskList, setTaskList } = useState([]);
+
+  useEffect(() => {
+    let array = localStorage.getItem("taskList");
+
+    if (array) {
+      setTaskList(JSON.parse(array));
+    }
+  }, []);
+
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: "todo",
+    drop: (item) =>
+      addToCompleted(
+        item.id,
+        item.projectName,
+        item.taskDescription,
+        item.timestamp,
+        item.duration
+      ),
+    collect: (monitor) => ({
+      isOver: !!monitor.isOver(),
+    }),
+  }));
+
+  const addToCompleted = (
+    id,
+    projectName,
+    taskDescription,
+    timeStamp,
+    duration
+  ) => {
+    const moveTask = taskList.filter((task) => id === task.id);
+    setCompleted((completed) => [
+      ...completed,
+      { moveTask, projectName, taskDescription, timeStamp, duration },
+    ]);
+  };
 
   return (
     <>
       <h1 className="text-2xl font-bold py-6 pl-5">03-Task Tracker</h1>
-        <p className="text-2xl font-bold py-4 pl-5">Hi there!</p>
+      <p className="text-2xl font-bold py-4 pl-5">Hi there!</p>
       <div className="flex flex-row items-center">
         <p className="text-xl pl-6">Click</p>
-        <AddTask taskList={taskList} setTaskList={setTaskList}/>
+        <AddTask taskList={taskList} setTaskList={setTaskList} />
         <p className="text-xl my-2">to add a new task</p>
       </div>
       <div>
-        <h2 className="ml-6 text-xl font-semibold w-3/4 max-w-lg my-4 py-2 px-4 bg-gray-300">To Do</h2>
-      {taskList.map((task, i) =>
-      <>
-        <ToDo key={i} task={task}  index={i} taskList={taskList} setTaskList={setTaskList} />
-      </>
-      )}
+        <h2 className="ml-6 text-xl font-semibold w-3/4 max-w-lg my-4 py-2 px-4 bg-gray-300">
+          To Do
+        </h2>
+        {taskList.map((task, i) => (
+          <>
+            <ToDo
+              key={i}
+              task={task}
+              index={i}
+              taskList={taskList}
+              setTaskList={setTaskList}
+            />
+          </>
+        ))}
+      </div>
+      <div className="w-full flex flex-col" ref={drop}>
+        <h2 className="text-xl font-semibold w-3/4 max-w-lg my-4 py-2 px-4 bg-gray-300">
+          Completed:
+        </h2>
+        {completed.map((task, i) => (
+          <ToDo
+            key={i}
+            task={task}
+            index={i}
+            taskList={taskList}
+            setTaskList={setTaskList}
+          />
+        ))}
       </div>
     </>
   );
--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,107 +1,106 @@
-import { useState } from "react"
+import { useState } from "react";
 
+const AddTask = ({ taskList, setTaskList }) => {
+  const [addModal, setAddModal] = useState(false);
+  const [projectName, setProjectName] = useState("");
+  const [taskDescription, setTaskDescription] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-const AddTask = ({taskList, setTaskList}) => {
-    const [addModal, setAddModal] = useState(false);
-    const [projectName, setProjectName] = useState("");
-    const [taskDescription, setTaskDescription] = useState("");
-    const [errorMessage, setErrorMessage] = useState("");
+  const handleInput = (e) => {
+    const { name, value } = e.target;
 
-    const handleInput = e => {
-
-        const { name, value} = e.target;
-
-        if (name === "projectName") { setProjectName(value) }
-        if (name === "projectName" && value === "") {
-            setErrorMessage("Enter project name to continue")
-        }
-        if (name === "taskDescription") setTaskDescription(value)
+    if (name === "projectName") {
+      setProjectName(value);
     }
-
-    const handleAdd = e => {
-        e.preventDefault();
-        if (!projectName) {
-            setErrorMessage("Enter project name to continue")
-        } else {
-        setTaskList(
-            [...taskList, {projectName, taskDescription}]
-        );
-        setAddModal(false);
-        setProjectName("");
-        setTaskDescription("");
+    if (name === "projectName" && value === "") {
+      setErrorMessage("Enter project name to continue");
     }
-}
+    if (name === "taskDescription") setTaskDescription(value);
+  };
 
-    return (
+  const handleAdd = (e) => {
+    e.preventDefault();
+    if (!projectName) {
+      setErrorMessage("Enter project name to continue");
+    } else {
+      setTaskList([...taskList, { projectName, taskDescription }]);
+      setAddModal(false);
+      setProjectName("");
+      setTaskDescription("");
+    }
+  };
+
+  return (
+    <>
+      <button
+        className="bg-blue-500 text-white uppercase text-sm font-semibold py-1 mx-1.5 pl-2 pr-2.5 rounded hover:opacity-70"
+        type="button"
+        onClick={() => setAddModal(true)}
+      >
+        + New Task
+      </button>
+      {addModal ? (
         <>
-        <button className="bg-blue-500 text-white uppercase text-sm font-semibold py-1 mx-1.5 pl-2 pr-2.5 rounded hover:opacity-70"
-            type="button"
-            onClick={() => setAddModal(true)}
-            >
-            + New Task</button>
-            {addModal ? (
-                <>
-                <div className="flex items-center justify-center overflow-x-hidden overflow-y-auto fixed inset-0 z-100">
-                    <div className="w-9/12 max-w-lg bg-white rounded-tg shaddow-md relative flex flex-col"> 
-                    <div className="flex flex-row justify-between p-5 border-b border-slate-200 rounded-t">
-                    <h3 className="text-3xl font-semibold">Add New Task</h3>
-                    <button
-                    className="px-1 text-gray-400 float-right text-3xl leading-none font-semibold block"
-                    onClick={() => setAddModal(false)}
-                    >
-                        x
-                    </button>
-                    </div>
-                    <form className="px-6 pt-6 pb-4">
-                        <div>
-
-                        <label className="track-wide uppercase text-gray-700 text-xx font-semibold mb-2 block"
-                        htmlFor="project-name">
-                            Project Name
-                        </label>
-                            <input
-                            className="w-full big-gray-200 test-grey-700 border border-gray-200 rounded py-3 px-4 mb-5 leading-tight focus:outline-none focus:bg-white"
-                                id="project-name"
-                                projectName="ProjectName"
-                                type="text"
-                                placeholder="project Name"
-                                value={projectName}
-                                onChange={handleInput}
-                                required
-                            />
-                            <p className="text-red-500 text-center mt-2 mb-5">{errorMessage}</p>
-                        </div>
-                        <div>
-                            <label htmlFor="">
-                                Task Description
-                            </label>
-                            <textarea 
-                            className="w-full big-gray-200 test-grey-700 border border-gray-200 rounded py-3 px-4 mb-5 leading-tight focus:outline-none focus:bg-white" 
-                            id="task-description"
-                            name="taskDescription"
-                            rows="5"
-                            placeholder="Task Description"
-                            value={taskDescription}
-                            onChange={handleInput}
-                            >
-
-                            </textarea>
-                        </div>
-                        </form>
-                        <div className="flex justify-center p-6 border-t border-slate-200 rounded-b">
-                            <button
-                            className="bg-blue-500 text-white font semi-bold uppercase text-sm px-6 py-3 rounded hover:opacity-70"
-                            onClick={handleAdd}
-                            >
-                                Add Task</button>
-                        </div>
-                    </div>
+          <div className="flex items-center justify-center overflow-x-hidden overflow-y-auto fixed inset-0 z-100">
+            <div className="w-9/12 max-w-lg bg-white rounded-tg shaddow-md relative flex flex-col">
+              <div className="flex flex-row justify-between p-5 border-b border-slate-200 rounded-t">
+                <h3 className="text-3xl font-semibold">Add New Task</h3>
+                <button
+                  className="px-1 text-gray-400 float-right text-3xl leading-none font-semibold block"
+                  onClick={() => setAddModal(false)}
+                >
+                  x
+                </button>
+              </div>
+              <form className="px-6 pt-6 pb-4">
+                <div>
+                  <label
+                    className="track-wide uppercase text-gray-700 text-xx font-semibold mb-2 block"
+                    htmlFor="project-name"
+                  >
+                    Project Name
+                  </label>
+                  <input
+                    className="w-full big-gray-200 test-grey-700 border border-gray-200 rounded py-3 px-4 mb-5 leading-tight focus:outline-none focus:bg-white"
+                    id="project-name"
+                    projectName="ProjectName"
+                    type="text"
+                    placeholder="project Name"
+                    value={projectName}
+                    onChange={handleInput}
+                    required
+                  />
+                  <p className="text-red-500 text-center mt-2 mb-5">
+                    {errorMessage}
+                  </p>
                 </div>
-                 
-                </>
-            ) : null}
+                <div>
+                  <label htmlFor="">Task Description</label>
+                  <textarea
+                    className="w-full big-gray-200 test-grey-700 border border-gray-200 rounded py-3 px-4 mb-5 leading-tight focus:outline-none focus:bg-white"
+                    id="task-description"
+                    name="taskDescription"
+                    rows="5"
+                    placeholder="Task Description"
+                    value={taskDescription}
+                    onChange={handleInput}
+                  ></textarea>
+                </div>
+              </form>
+              <div className="flex justify-center p-6 border-t border-slate-200 rounded-b">
+                <button
+                  className="bg-blue-500 text-white font semi-bold uppercase text-sm px-6 py-3 rounded hover:opacity-70"
+                  onClick={handleAdd}
+                >
+                  Add Task
+                </button>
+              </div>
+            </div>
+          </div>
         </>
-    )
-}
+      ) : null}
+    </>
+  );
+};
 
 export default AddTask;
